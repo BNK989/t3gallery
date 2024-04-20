10697c426580ac5e63303677253c9f716ba860aa
Ben <bnk989@gmail.com>
2024-04-20 19:16:04 +0300

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import Link from "next/link";
 
+const mockUrls = [
+  'https://unsplash.../.png',
+  'https://unsplash.../.png',
+  'https://unsplash.../.png',
+  'https://unsplash.../.png',
+]
+
+const mockImages = mockUrls.map((url,i) =>({
+  id: i + 1,
+  url,
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-    Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">{
+        [...mockImages, ...mockImages, ...mockImages].map((img) => (
+          <div key={img.id} className="w-48 p-4">
+            <img src={img.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
